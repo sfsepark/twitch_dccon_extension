62830146058744bfd91d525faeede4636198a3a0
sfsepark <sfsepark@gmail.com>
2019-06-27 19:25:06 +0900

    1.3.5
    트위치 신버전을 지원합니다!
    이제 역가(https://twitch.tv/yeokka) 방송을 비롯한 모든 opendccon을 사용하는 방송에서 지원됩니다!
    새로고침 않아도 어떠한 페이지에서든 디시콘을 이용하는 곳이라면 정상작동됩니다.
    이제, 자유롭게 채널을 이동하세요!

--- content_scripts/dccon_searcher.js
+++ content_scripts/dccon_searcher.js
@@ -1,11 +1,12 @@
 /*
 TWITCH DCCON EXTENSTION 
-version 1.0.0
+version 1.3.5
 
-developer : 
-sfsepark@gmail.com
-https://www.twitch.tv/funzzinuu 
+Main developer : sfsepark@gmail.com
+DCCON selector developer : funzinnu@gmail.com
+Jassist-Open-DCCON developer : rishubil@gmail.com
 */
+
 
 //data for chat-crawling
 var chatMessage = "";
@@ -14,15 +15,6 @@ var chatMessage = "";
 
 var DCCONJSON = null;
 var ONOFF = true;
-
-chrome.runtime.sendMessage({
-    method: 'POST',
-    action: 'xhttp',
-    url: 'http://funzinnu.cafe24.com/stream/dccon.php',
-    data: ''
-}, function(responseText) {
-    DCCONJSON = JSON.parse(responseText);
-});
 
 chrome.runtime.sendMessage({
     method: 'POST',
@@ -57,7 +49,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
 var cur_li_id = 'emberXXXX';
 var next_li_id = 'emberXXXX';
 
-var reg = /^\s*~(\S+)\s*((\S|\s)*)/;
+//var reg = /((?:\S|\s)*)~(\S+)\s*((?:\S|\s)*)/;
+var reg = /~(\S+)\s*/;
 
 //dummy target
 var target = document;
@@ -69,35 +62,63 @@ var config = { attributes: true, childList: true, characterData: true };
 
 var addChatDetectObserver = function()
 {
-    observer.disconnect();
-
-    //origin
+    //origin(regacy)
     target_lists = document.getElementsByClassName('chat-lines');
-    chatMessage = 'message';
-    if(target_lists.length == 0)
+ 
+    if(target_lists.length != 0)
     {
-        //twitch_beta 
+        chatMessage = 'message';
+    }
+    else
+    {
+        //twitch(new) 
         target_lists = document.getElementsByClassName('chat-list__lines');
-        chatMessage = 'chat-message-text';
+
+        if(target_lists.length != 0)
+        { 
+            target_lists = target_lists[0].getElementsByClassName('simplebar-content');
+            target_lists = target_lists[0].getElementsByClassName('full-height');
+            
+            if(target_lists.length == 0)
+            {
+                target_lists = document.getElementsByClassName('chat-list__lines');
+                target_lists = target_lists[0].getElementsByClassName('simplebar-content');
+                target_lists = target_lists[0].getElementsByClassName('tw-full-height');
+            }
+            chatMessage = 'chat-message-text';
+        }
+        else
+        {
+            //clip,video chatting
+            target_lists = document.getElementsByClassName('qa-vod-chat');
+            if(target_lists.length != 0)
+            {
+                target_lists = target_lists[0].getElementsByClassName('tw-align-items-end');
+                chatMessage = 'qa-mod-message';
+            }
+            else
+            {
+                chatMessage = '';
+            }
+        }
     }
-    if(target_lists.length == 0)
-    { 
-        //clip,video chatting
-        target_lists = document.getElementsByClassName('video-chat--full-height')[0].getElementsByClassName('align-items-end');
-        chatMessage = 'qa-mod-message';
-    }
+
 
     if(target_lists.length != 0)
     {
-        target = target_lists[0];
+        if(target != target_lists[0])
+        {
+            observer.disconnect();  
 
-        // create an observer instance
-        observer = new MutationObserver(function(mutations) {
-            chat_detect();  
-        });
-
-        observer.observe(target, config);
-        clearInterval(observeInterval);
+            target = target_lists[0];
+            
+            // create an observer instance
+            observer = new MutationObserver(function(mutations) {
+                chat_detect();  
+            });
+    
+            observer.observe(target, config);
+        }  
     }
  
 }
@@ -113,7 +134,7 @@ var chat_detect = function(){
         
             var len = target.children.length;
 
-            for(var i = len ; i -- ; i >=0)
+            for(var i = len - 1 ; i >= 0 ; i --)
             {
                 var child =  target.children[i];
 
@@ -138,28 +159,31 @@ var chat_detect = function(){
         }
         else if(chatMessage == 'chat-message-text')
         {
-            for(var i = target.children.length - 3; i -- ; i == 0)
-            {
-                var child = target.children[i];
+            var child = target.children;
+            var len = child.length;
 
-                if(child.getAttribute('dccon_changed') == 'true')
+            for(var i = len - 1 ; i >= 0 ; i --)
+            {
+                if(child[i].getAttribute('dccon_changed') == 'true')
                 {
                     break;
                 }
                 else{
-                    child.setAttribute('dccon_changed','true');
+                    child[i].setAttribute('dccon_changed','true');
 
-                    for(var c in child.classList)
+                    for(var c in child[i].classList)
                     {
-                        if(child.classList[c] == "chat-line__message")
-                            edit_chating(child);              
+                        if(child[i].classList[c] == "chat-line__message")
+                            edit_chating(child[i]);              
+                            break;
                     }
-                }               
-            }
+                }            
+            }  
+
         }
         else if(chatMessage == 'qa-mod-message')
         {
-            for(var i = target.children.length; i -- ; i == 0)
+            for(var i = target.children.length - 1; i >= 0; i --)
             {
                 var child = target.children[i];
 
@@ -178,41 +202,254 @@ var chat_detect = function(){
 
 var edit_chating = function(chatLI)
 {
-    var messageSpan ;
+    if(ONOFF == true)
+    {
+        var messageSpan = [] ;
 
-    if(chatMessage == 'message')
-    {
-        messageSpan = chatLI.getElementsByClassName(chatMessage)[0];
-    }
-    else if(chatMessage == 'chat-message-text')
-    {
-        for(var c in chatLI.children)
+        if(chatMessage == 'message')
         {
-            if(chatLI.children[c].getAttribute('data-a-target') == chatMessage)
+            messageSpan.push(chatLI.getElementsByClassName(chatMessage)[0]);
+        }
+        else if(chatMessage == 'chat-message-text')
+        {
+            var len = chatLI.children.length
+
+            for(i = 0 ; i < len ; i ++)
             {
-                messageSpan = chatLI.children[c] ;
-                break;
+                if(chatLI.children[i].getAttribute('data-a-target') == chatMessage)
+                {
+                    messageSpan.push(chatLI.children[i]);
+                }
+            }
+            
+        }
+        else if(chatMessage == 'qa-mod-message')
+        {
+            var tmpSpan = chatLI.getElementsByClassName(chatMessage)[0].children;
+            for(i = 0 ;i < tmpSpan.length; i ++){
+                if(tmpSpan[i].getAttribute('data-a-target') != 'emote-name')
+                {
+                    messageSpan.push(tmpSpan[i]);
+                }
             }
         }
-    }
-    else if(chatMessage == 'qa-mod-message')
-    {
-        messageSpan = chatLI.getElementsByClassName(chatMessage)[0];
-        messageSpan = messageSpan.children[0];
-    }
-    
 
-    var result = reg.exec(messageSpan.innerHTML);
-    if(result != null && DCCONJSON[result[1]] != undefined && ONOFF == true)
-    {
-        messageSpan.innerHTML = "<div style =\"height : 10px;\"></div><div style =\"margin-top:2px margin-bottom:2px\"><span class=\"balloon-wrapper\"><img src=\"" + 
-        DCCONJSON[result[1]] + "\" alt=\"~" + result[1] + "\" class=\"emoticon\"><div class=\"balloon balloon--tooltip balloon--up balloon--center mg-t-1\">~"+ result[1] +"</div></span></div><div style =\"height : 10px;\"></div>" + result[2];
+        for(i = 0 ; i < messageSpan.length ; i ++)
+        {
+            var cur_html = '';
+            var cur_text = messageSpan[i].innerText;
+            var result = cur_text.split(reg);
+
+            for(var j = 0 ; j < result.length ; j ++)
+            {
+                if(j %2 == 1)   {
+                    cur_html += DCCON_converted(result[j]);
+                }
+                else{
+                    cur_html += htmlEntityEnc(result[j]);
+                }
+            }
+
+            messageSpan[i].innerHTML = cur_html;
+        }
+
+
     }
 }
 
+var ratio = 100.0;
+
+// style=\"width:20%; height:20%\"
+
+var DCCON_converted = function(Text)
+{
+    var converted_Text = Text;
+
+    var width = 100 * ratio / 100;
+    var height = 100 * ratio / 100;
+
+    var cur_dccon ; 
+
+    if(streamer == 'funzinnu')
+    {
+        if(DCCONJSON[Text] != undefined)   {
+            cur_dccon = DCCONJSON[Text];
+        }
+        else {
+            return '~' + converted_Text;
+        }
+    }
+    else
+    {
+        cur_dccon = GET_DCCON(Text);
+
+        if(cur_dccon == undefined ) 
+            return '~' + converted_Text;
+    }
+    /*
+
+    <div class="tw-tooltip-wrapper inline" data-a-target="emote-name"><img src="http://puu.sh/sYGQM/1887cb7d57.gif" alt="~우리핵박수1" class="emoticon" style="width:100px; height:100px"><div class="tw-tooltip tw-tooltip--up tw-tooltip--align-center" data-a-target="tw-tooltip-label" style="margin-bottom: 0.9rem; display: block; visibility: visible;">~우리핵박수1</div></div>
+*/
+
+    messageHTML = "<div style =\"height : 0px;\"></div><div style =\"margin-top:4px; margin-bottom:4px; height:" + height + "px;\"><div class=\"tw-tooltip-wrapper inline\" data-a-target=\"emote-name\"><img src=\"" + 
+    cur_dccon + "\" alt=\"~" + Text + "\" class=\"emoticon\" style=\"width:" + width + "px; height:" + height +"px\"><div class=\"tw-tooltip tw-tooltip--up tw-tooltip--align-center\" data-a-target=\"tw-tooltip-label\" style=\"margin-bottom: 0.9rem;\">~"+ Text +"</div></div></div><div style =\"height : 0px;\"></div>";    
+    converted_Text = messageHTML;
+   
+    return converted_Text;
+}
+
+var GET_DCCON = function(str)
+{
+    var dccons = DCCONJSON.dccons;
+    var len = dccons.length;
+
+    for(var i = 0 ; i < len ; i ++)
+    {
+        key_len = dccons[i]['keywords'].length;
+        for(var j = 0 ; j < key_len ; j ++)
+        {
+            if(dccons[i]['keywords'][j] == str)
+            {
+                return dccons[i]['path'];
+            }
+        }
+    }
+
+    return null;
+}
+
+function htmlEntityEnc(str){
+    if(str == "" || str == null){
+        return str;
+    }
+    else{
+        return str.replace("&", "&amp;").replace("#", "&#35;").replace("<", "&lt;").replace(">", "&gt;").replace(/"/g, "&quot;").replace('\\', "&#39;").replace('%', "&#37;").replace('(', "&#40;").replace(')', "&#41;").replace('+', "&#43;").replace('/', "&#47;").replace('.', "&#46;");
+    }
+}
 
 //-------------------start of routine---------------------------
 
-var observeInterval = setInterval(function(){
-    addChatDetectObserver();
-}, 3000);
+var streamer = '';
+
+var observeInterval;
+
+var chat_header_class = [['chat__header', 'chat__header-channel-name'], ['chat-room__header', 'chat-room__header-channel-name']];
+
+var channelDectectInterval = setInterval(function() {
+    
+    var cur_streamer = '';
+
+    //chat_header
+    var chat_header = [];
+    
+    for(var index = 0 ; index < chat_header_class.length ; index ++)
+    {
+        chat_header = document.getElementsByClassName(chat_header_class[index][0]);
+
+        if(chat_header.length != 0)
+        {
+            var len = chat_header[0].children.length
+
+            for(var i = 0 ;i < len ; i ++)
+            {
+                if(chat_header[0].children[i].getAttribute('data-a-target') == chat_header_class[index][1])
+                {
+                    cur_streamer = chat_header[0].children[i].innerText;
+                    break;
+                }
+            }
+        }
+    }
+
+    //channel_header
+    if(cur_streamer == '')
+    {
+        var channel_header = document.getElementsByClassName('channel-header');
+    
+        if(channel_header.length != 0)
+        {
+            var channel_header_user = channel_header[0].getElementsByClassName('channel-header__user')[0];
+            var channel_user_name = channel_header_user.getAttribute('href');
+            try{
+                cur_streamer = channel_user_name.substring(1);
+            } catch(err){}
+        }
+    }
+    
+    // from URL
+    if(cur_streamer == '')
+    {
+        var url = location.href;
+        var path_name_regex = /(^[^&?#]+)/g;
+        var res = url.match(path_name_regex);
+        var url_regex = /\/([^\/]+)/g;
+        res = res[0].match(url_regex);
+
+        try{
+            if(res.length > 0)
+                if(res[1] == '/popout')
+                {
+                    cur_streamer = res[2].substring(1);
+                }
+                else{
+                    cur_streamer = res[1].substring(1);
+                }   
+        } catch(err){}
+    }
+
+    if(cur_streamer == '')
+    {
+        streamer = '';
+        DCCONJSON = null;
+        observer.disconnect();
+        clearInterval(observeInterval);
+    }
+    else
+    {
+        if(cur_streamer != streamer)
+        {
+            var JSONURL = '';
+            streamer = cur_streamer;
+
+            DCCONJSON = null;
+            observer.disconnect();
+            clearInterval(observeInterval);
+            target = document;
+
+            chrome.runtime.sendMessage({
+                method: 'GET',
+                action: 'xhttp',
+                url: 'https://open-dccon-selector.update.sh/api/dccon-url?channel_name=' + cur_streamer,
+                data: ''
+            }, function(responseText) {
+                JSONURL = JSON.parse(responseText);
+
+                if(JSONURL['user_id'] != null)
+                {
+                    observeInterval = setInterval(function(){
+                        addChatDetectObserver();
+                    }, 2000);
+
+                    if(cur_streamer == 'funzinnu') {
+                        JSONURL = 'http://funzinnu.cafe24.com/stream/dccon.php';
+                    }
+                    else  {
+                        JSONURL = JSONURL['dccon_url'];
+                    }
+
+                    chrome.runtime.sendMessage({
+                        method: 'GET',
+                        action: 'xhttp',
+                        url: JSONURL,
+                        data: ''
+                    }, function(responseText) {
+                        DCCONJSON = JSON.parse(responseText);
+                    });
+                }
+            });
+        }
+    }
+    
+}, 1500)
+
+//--------------------end of routine---------------------------------
