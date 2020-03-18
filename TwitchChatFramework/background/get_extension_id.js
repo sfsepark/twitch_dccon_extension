90a988365ff6ecedb92446dd4b2fc4b0284da289
sfsepark <sfsepark@gmail.com>
2020-03-19 06:14:40 +0900

    파이어폭스 지원 - extension_id에 정규식 지원
    이슈 #4 해결

--- TwitchChatFramework/background/get_extension_id.js
+++ TwitchChatFramework/background/get_extension_id.js
@@ -1,7 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, callback){
     if(request.type == 'get_extension_id'){
         chrome.management.getSelf(function(result){
-            callback({data:true, extension_id : result.id});
+            let uuid_reg = /[0-9a-zA-Z\-]+/;
+            let reg_res =  uuid_reg.exec(result);
+            let extension_id = result.id;
+            if(reg_res != null){
+                extension_id = reg_res[0];
+            }
+            callback({data:true, extension_id : extension_id});
 
             return true;
         })
